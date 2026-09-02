b8217cc30d0acb255a48698adb6dd8fb4ef4d9b5
Ishnazarov Saidnazar <ishnazarov.saidnazar@micros.uz>
2026-09-02 18:52:19 +0500

    init commit

---
+++ Application/Authentication/Dto/AuthRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Authentication.Dto;
+
+public record AuthRequest
+{
+    public string Username { get; init; } = null!;
+    public string Password { get; init; } = null!;
+}
---
+++ Application/Authentication/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿using Application.Authentication.Dto;
+
+namespace Application.Authentication;
+
+public interface IAuthService
+{
+    bool IsAuthenticated(AuthRequest request);
+}
