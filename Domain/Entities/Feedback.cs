d8fb3769d585f5bc54a86be0259b4cbbef31a503
Ishnazarov Saidnazar <ishnazarovsaidnazar24@gmail.com>
2026-09-02 22:06:33 +0500

    added infrastructure project

--- Domain/Entities/Feedback.cs
+++ Domain/Entities/Feedback.cs
@@ -1,6 +1,10 @@
-﻿namespace Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
 
 public class Feedback
 {
-
+    public int Id { get; set; }
+    [MaxLength(500)] public string Title { get; set; } = null!;
+    
 }
---
+++ Infrastructure/Repositories/FeedbackRepository.cs
@@ -0,0 +1,8 @@
+﻿using Application.Feedback;
+
+namespace Infrastructure.Repositories;
+
+public class FeedbackRepository : IFeedbackRepository
+{
+    
+}
